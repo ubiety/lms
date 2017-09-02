834730a667042511efa365b0781b57278d1dd184
Dieter Lunn <coder2000@gmail.com>
2017-09-02 11:33:04 -0500

    Add user and session controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+# Sessions controller
+# used to sign a user in and out
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if (@user = login(params[:email], params[:password]))
+      redirect_back_or_to(:users, success: 'Logged in successfully')
+    else
+      Flash.now[:error] = 'Login failed'
+      render action: :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:users, success: 'Logged out successfully')
+  end
+end
