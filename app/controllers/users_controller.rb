a0fdf5019baae4a10db41e64119778f079cb7fe7
Dieter Lunn <coder2000@gmail.com>
2017-09-03 00:25:56 -0500

    Add signup actions to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 # Users controller
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if user.save!
+      redirect_to @user, flash: { success: 'User signed up successfully' }
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    require(:user).permit(:email, :name, :role, :password,
+                          :password_confirmation, :first_name, :middle_name,
+                          :last_name)
+  end
 end
