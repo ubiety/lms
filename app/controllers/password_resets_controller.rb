7241ce8a275458fbee14dfe8afa7cd028a5574c0
Dieter Lunn <coder2000@gmail.com>
2017-09-05 01:40:15 -0500

    Add password reset controller

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,41 @@
+# Password reset controller
+class PasswordResetsController < ApplicationController
+  def create
+    @user = User.find_by_email(params[:email])
+
+    @user.deliver_password_reset_instructions! if @user
+
+    redirect_to login_path, notice: 'Instructions have been sent to your email.'
+  end
+
+  def edit
+    @token = params[:id]
+    @user = User.load_from_password_reset_token(@token)
+
+    not_authenticated if @user.blank?
+  end
+
+  def update
+    @token = params[:id]
+    @user = User.load_from_password_reset_token(@token)
+
+    if @user.blank?
+      not_authenticated
+      return
+    end
+
+    update_password params[:user][:password],
+                    params[:user][:password_confirmation]
+  end
+
+  private
+
+  def update_password(password, confirmation)
+    @user.password_confirmation = confirmation
+    if @user.change_password!(password)
+      redirect_to login_path, notice: 'Password changed successfully'
+    else
+      redirect_to action: :edit
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'password_resets/create'
+
+  get 'password_resets/edit'
+
+  get 'password_resets/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: %w[new]
 
