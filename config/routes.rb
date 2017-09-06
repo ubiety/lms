843783a01a993e439c7f1ba527e0543c590dacd7
Dieter Lunn <coder2000@gmail.com>
2017-09-06 03:38:00 -0500

    Add dashboard and admin menu

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: :login
   get '/logout', to: 'sessions#destroy', as: :logout
 
-  root to: 'users#index'
+  root to: 'pages#dashboard'
 end
