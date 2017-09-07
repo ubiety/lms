836bc97812cd4dbc8a48f7be47df0d3c67bee906
Dieter Lunn <coder2000@gmail.com>
2017-09-07 16:14:42 -0500

    Setup CI and coverage

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,8 +4,10 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
+require 'coveralls'
 require 'cucumber/rails'
 
+Coveralls.wear_merged!('rails')
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
@@ -55,4 +57,3 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
-
