314f36b12622b4f2542274883c86c8f69c83745b
Dieter Lunn <coder2000@gmail.com>
2017-09-07 17:45:14 -0500

    Add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    before { FactoryGirl.build(:user) }
+
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_presence_of(:password_confirmation) }
+    it { is_expected.to validate_confirmation_of(:password) }
+    it { is_expected.to validate_presence_of(:role) }
+    it { is_expected.to define_enum_for(:role) }
+    it { is_expected.to have_many(:enrolments) }
+    it { is_expected.to have_many(:courses).through(:enrolments) }
+  end
 end
