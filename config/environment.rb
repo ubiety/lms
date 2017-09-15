c0741a2b650fff36b12a5781c7caaef291059fe3
Dieter Lunn <coder2000@gmail.com>
2017-09-14 19:59:53 -0500

    Add logentries

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if !Rails.env.development?
+  Rails.logger = Le.new('291ece5e-34d1-4e42-9763-463a8196fd58')
+else
+  Rails.logger = Le.new('291ece5e-34d1-4e42-9763-463a8196fd58', local: true)
+end
