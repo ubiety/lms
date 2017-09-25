9bab984d9f0ff7d0a087d2b22918c109c9284ed9
Dieter Lunn <coder2000@gmail.com>
2017-09-25 12:46:37 -0500

    Refactor with haml-lint and longer line length

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,10 +1,8 @@
 # Assignment helper methods
 module AssignmentsHelper
   def render_description(description)
-    options = { autolink: true, tables: true, hard_wrap: true,
-                no_intra_emphasis: true, fenced_code: true, gh_blockcode: true,
-                underline: true }
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
-                       .render(description).html_safe
+    options = { autolink: true, tables: true, hard_wrap: true, no_intra_emphasis: true,
+                fenced_code: true, gh_blockcode: true, underline: true }
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(description).html_safe
   end
 end
