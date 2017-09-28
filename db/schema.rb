1ea67340f26c36a93fd632b5085f9351509106cc
Dieter Lunn <coder2000@gmail.com>
2017-09-28 01:39:38 -0500

    Readd settings controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928055737) do
+ActiveRecord::Schema.define(version: 20170928063431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,9 +115,13 @@ ActiveRecord::Schema.define(version: 20170928055737) do
   end
 
   create_table "settings", force: :cascade do |t|
-    t.string "brand"
+    t.string "var", null: false
+    t.text "value"
+    t.integer "thing_id"
+    t.string "thing_type", limit: 30
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true
   end
 
   create_table "users", force: :cascade do |t|
