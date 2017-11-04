6c723c1e2af186d64ae0ea331380fa208302eedd
Dieter Lunn <coder2000@gmail.com>
2017-11-04 11:50:47 -0500

    Add webpack and react

---
+++ app/javascript/components/Chat.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Chat extends React.Component {
+  render () {
+    return (
+      <div class="card">
+        <div class="card-header">Chat</div>
+      </div>
+    )
+  }
+}
+
+export default Chat
