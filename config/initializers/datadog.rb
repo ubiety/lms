3b92ed0213271961f2c7d04410f84597206e92d0
Dieter Lunn <coder2000@gmail.com>
2017-11-16 00:51:53 -0600

    Add environment variable gem and update sorcery version

--- config/initializers/datadog.rb
+++
@@ -1,8 +0,0 @@
-# config/initializers/datadog-tracer.rb
-
-Rails.configuration.datadog_trace = {
-  enabled: Rails.env.test? ? false : true,
-  auto_instrument: true,
-  auto_instrument_redis: true,
-  default_service: 'lms'
-}
