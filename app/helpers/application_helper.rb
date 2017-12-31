f3c99ee7c0ff24caa8bacbfb3e851c50131fcc07
Dieter Lunn <coder2000@gmail.com>
2017-12-31 10:34:01 -0600

    Move flash message rendering to partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,5 @@
 # Application helper
 module ApplicationHelper
-  KLASS = 'alert alert-dismissable fade show'.freeze
-
-  def flash_messages(_opts = {})
-    flash.each do |msg_type, message|
-      render partial: 'shared/flash', locals: { classes: "#{class_for(msg_type)} #{KLASS}",
-                                                message: message }
-    end
-  end
-
   def class_for(flash_type)
     {
       success: 'alert-success',
