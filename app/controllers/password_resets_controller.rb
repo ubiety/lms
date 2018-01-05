d9c1d50e1c23c8279356980eebfb72676675edca
Dieter Lunn <coder2000@gmail.com>
2018-01-05 01:17:31 -0600

    Update reset password process

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,23 +1,26 @@
 # Password reset controller
 class PasswordResetsController < ApplicationController
+  skip_before_action :require_login
+  layout false
+
   def create
     @user = User.find_by_email(params[:email])
 
-    @user.deliver_password_reset_instructions! if @user
+    @user.deliver_reset_password_instructions! if @user
 
     redirect_to login_path, notice: 'Instructions have been sent to your email.'
   end
 
   def edit
     @token = params[:id]
-    @user = User.load_from_password_reset_token(@token)
+    @user = User.load_from_reset_password_token(@token)
 
     not_authenticated if @user.blank?
   end
 
   def update
     @token = params[:id]
-    @user = User.load_from_password_reset_token(@token)
+    @user = User.load_from_reset_password_token(@token)
 
     if @user.blank?
       not_authenticated
